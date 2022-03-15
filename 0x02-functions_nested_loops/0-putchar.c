8520d08ec8054f6c89050121b83cf79ec4051f00
hefeholuwah <ilieas300@gmail.com>
2022-03-15 09:31:12 -0700

    gd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
-/**
- * main -  prints Holberton, followed by a new line
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
-    _putchar(_);
+_putchar(_);
 _putchar(p);
 _putchar(u);
 _putchar(t);
@@ -15,6 +12,7 @@ _putchar(h);
 _putchar(a);
 _putchar(r);
 _putchar(n);
+
 
 return (0);
 }
