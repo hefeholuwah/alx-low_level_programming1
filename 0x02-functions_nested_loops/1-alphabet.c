57d2ea54912ffe82f25c9395af59a173d645e96e
hefeholuwah <ilieas300@gmail.com>
2022-03-16 08:54:30 -0700

    prints small letters of alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
-
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
 void print_alphabet(void)
 {
-char ch;
+	char ch;
 
-ch = a;
+	ch = 'a';
 
-while (ch <= z)
-{
-_putchar(ch);
-ch++;
-}
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
 
-_putchar(n);
+	_putchar('\n');
 }
